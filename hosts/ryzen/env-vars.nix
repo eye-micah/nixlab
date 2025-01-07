5f06a99354ab48f4a0f322c4e4ceb49c88793057
Micah C <kenzan@mailbox.org>
2025-01-07 13:14:15 -0500

    Set pool name.

--- hosts/ryzen/env-vars.nix
+++ hosts/ryzen/env-vars.nix
@@ -1,6 +1,7 @@
 { config, pkgs, lib, ... }:
 
 {
+    zfsSolidStatePool = "storage-ssd";
     dockerPersist = lib.mkDefault "/mnt/persist/docker";
     moviesDir = lib.mkDefault "/mnt/${zfsSolidStatePool}/movies";
     tvDir = lib.mkDefault "/mnt/${zfsSolidStatePool}/tv";
