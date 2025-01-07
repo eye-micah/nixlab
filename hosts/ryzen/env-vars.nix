802656cbf69d33b4939316e01b2a09f5206eaefc
Micah C <kenzan@mailbox.org>
2025-01-07 14:31:51 -0500

    I just needed to remove the function crap at the start lol

--- hosts/ryzen/env-vars.nix
+++ hosts/ryzen/env-vars.nix
@@ -1,37 +1,8 @@
-{ config, pkgs, lib, ... }:
-
 let
-  zfsSolidStatePool = "storage-ssd";
+    zfsSolidStatePool = "storage-ssd";
 in
 {
-  options = {
-    zfsSolidStatePool = lib.mkOption {
-      type = lib.types.str;
-      default = zfsSolidStatePool;
-      description = "The name of the ZFS pool.";
-    };
-
-    dockerPersist = lib.mkOption {
-      type = lib.types.str;
-      default = "/mnt/persist/docker";
-      description = "Directory for persistent Docker data.";
-    };
-
-    moviesDir = lib.mkOption {
-      type = lib.types.str;
-      default = "/mnt/${zfsSolidStatePool}/movies";
-      description = "Directory where movies are stored.";
-    };
-
-    tvDir = lib.mkOption {
-      type = lib.types.str;
-      default = "/mnt/${zfsSolidStatePool}/tv";
-      description = "Directory where TV shows are stored.";
-    };
-  };
-
-  config = {
-    # If you want to use these options directly in the config
-    # e.g., `config.moviesDir`, etc.
-  };
+    dockerPersist =  "/mnt/persist/docker";
+    moviesDir =  "/mnt/${zfsSolidStatePool}/movies";
+    tvDir =  "/mnt/${zfsSolidStatePool}/tv";
 }
