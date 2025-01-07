65e47ea2ab02675e62fcf6c3c8b293a45dd62b63
Micah C <kenzan@mailbox.org>
2025-01-07 14:21:53 -0500

    ChatGPT assistance

--- hosts/ryzen/env-vars.nix
+++ hosts/ryzen/env-vars.nix
@@ -1,9 +1,37 @@
 { config, pkgs, lib, ... }:
-let 
-    zfsSolidStatePool = "storage-ssd";
+
+let
+  zfsSolidStatePool = "storage-ssd";
 in
 {
-    dockerPersist = lib.mkDefault "/mnt/persist/docker";
-    moviesDir = lib.mkDefault "/mnt/${zfsSolidStatePool}/movies";
-    tvDir = lib.mkDefault "/mnt/${zfsSolidStatePool}/tv";
+  options = {
+    zfsSolidStatePool = lib.mkOption {
+      type = lib.types.str;
+      default = zfsSolidStatePool;
+      description = "The name of the ZFS pool.";
+    };
+
+    dockerPersist = lib.mkOption {
+      type = lib.types.str;
+      default = "/mnt/persist/docker";
+      description = "Directory for persistent Docker data.";
+    };
+
+    moviesDir = lib.mkOption {
+      type = lib.types.str;
+      default = "/mnt/${zfsSolidStatePool}/movies";
+      description = "Directory where movies are stored.";
+    };
+
+    tvDir = lib.mkOption {
+      type = lib.types.str;
+      default = "/mnt/${zfsSolidStatePool}/tv";
+      description = "Directory where TV shows are stored.";
+    };
+  };
+
+  config = {
+    # If you want to use these options directly in the config
+    # e.g., `config.moviesDir`, etc.
+  };
 }
