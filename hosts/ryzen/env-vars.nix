832e91a84690dc86d197fa2d8437801810491f77
Micah C <kenzan@mailbox.org>
2025-01-07 14:19:39 -0500

    Used let binding

--- hosts/ryzen/env-vars.nix
+++ hosts/ryzen/env-vars.nix
@@ -1,7 +1,8 @@
 { config, pkgs, lib, ... }:
-
-{
+let 
     zfsSolidStatePool = "storage-ssd";
+in
+{
     dockerPersist = lib.mkDefault "/mnt/persist/docker";
     moviesDir = lib.mkDefault "/mnt/${zfsSolidStatePool}/movies";
     tvDir = lib.mkDefault "/mnt/${zfsSolidStatePool}/tv";
