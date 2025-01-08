3dc9a3485d495f4a4038a733a04d2a829f1d102c
Micah C <kenzan@mailbox.org>
2025-01-07 19:04:07 -0500

    iyea

--- hosts/ryzen/configuration.nix
+++ hosts/ryzen/configuration.nix
@@ -131,10 +131,13 @@
         home = "/home/micah";
         password = "micah";  # This will use a hashed password in the system
         shell = pkgs.bash;    # Set the shell to Zsh or another shell of your choice
+        group = "micah";
         extraGroups = [ "wheel" "podman" "video" "input" ];
         openssh.authorizedKeys.keys = [
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHFQy6Jw3QC3ADSbNdRZZSTZMOwB7o/+SQatG4Er2gtC micah@haruka.tail8d76a.ts.net"
         ];
       };
+
+      users.groups.micah = {};
 
 }
