6faeffe805789d63d476ec3988c183afe18e546e
Micah C <kenzan@mailbox.org>
2025-01-12 00:01:18 -0500

    home?

--- home-manager/clients/linux.nix
+++ home-manager/clients/linux.nix
@@ -5,7 +5,7 @@
   home.homeDirectory = "/home/micah";  # Set Linux home directory
 
   # Other Linux-specific configurations can go here
-  dconf = {
+  home.dconf = {
     enable = true;
     settings = {
       "org/gnome/shell" = {
