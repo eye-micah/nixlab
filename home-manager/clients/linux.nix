036465ee9b023581ecdcbcc3f4967e904f796735
Micah C <kenzan@mailbox.org>
2025-01-12 00:03:01 -0500

    home > h-m for directry

--- home-manager/clients/linux.nix
+++ home-manager/clients/linux.nix
@@ -2,7 +2,7 @@
 { config, pkgs, lib, ... }:
 
 {
-  home.homeDirectory = "/home/micah";  # Set Linux home directory
+  home-manager.homeDirectory = "/home/micah";  # Set Linux home directory
 
   # Other Linux-specific configurations can go here
   home-manager.dconf = {
