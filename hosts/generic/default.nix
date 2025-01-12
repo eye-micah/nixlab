73e23f2ba35a32b2ee6f531035c14eed5dc418de
Micah C <kenzan@mailbox.org>
2025-01-12 02:05:42 -0500

    yea

--- hosts/generic/default.nix
+++ hosts/generic/default.nix
@@ -3,7 +3,7 @@
 {
   
   networking.hostName = lib.mkDefault "nixos";
-  networking.hostId = builtins.substring 0 8 (builtins.hashString "sha256" config.system.hostname);
+  networking.hostId = builtins.substring 0 8 (builtins.hashString "sha256" config.networking.hostName);
 
   networking.useDHCP = true;
 
