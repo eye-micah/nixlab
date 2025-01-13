cdffb7faa96874a9346a2ef67304b287bd282395
Micah C <kenzan@mailbox.org>
2025-01-13 01:19:57 -0500

    chatgpt cleaned it up lol

--- flake.nix
+++ flake.nix
@@ -1,153 +1,108 @@
 {
-    inputs = {
-        # I like living on the edge, and going with unstable just means less headaches with out of date software
-        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-        # Used for partitioning
-        disko.url = "github:nix-community/disko";
-        disko.inputs.nixpkgs.follows = "nixpkgs";
-        # Used for deploying to nodes
-        deploy-rs.url = "github:serokell/deploy-rs";
-        deploy-rs.inputs.nixpkgs.follows = "nixpkgs";
-        # Script from Wolfgang's Channel for forcing ASPM.
-        auto-aspm = {
-            url = "github:notthebee/AutoASPM";
-            flake = false;
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # Helper to follow nixpkgs
+    followNixpkgs = input: { url = input; inputs.nixpkgs.follows = "nixpkgs"; };
+
+    disko = followNixpkgs "github:nix-community/disko";
+    deploy-rs = followNixpkgs "github:serokell/deploy-rs";
+    nix-darwin = followNixpkgs "github:LnL7/nix-darwin/master";
+    home-manager = followNixpkgs "github:nix-community/home-manager/master";
+    impermanence.url = "github:nix-community/impermanence";
+    auto-aspm = { url = "github:notthebee/AutoASPM"; flake = false; };
+  };
+
+  outputs = { self, nixpkgs, nix-darwin, home-manager, ... } @ inputs:
+    let
+      # Common definitions
+      defaultSystem = "x86_64-linux";
+
+      diskoImport = path: import path { device = "IDK"; };
+
+      micahHomeConfig = {
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+        home-manager.users.micah = import ./home-manager/clients/home.nix;
+      };
+
+      commonModules = [
+        ./modules/zfs.nix
+        ./hosts/configuration.nix
+        ./modules/zfs-fs-config.nix
+      ];
+
+      hosts = {
+        generic = {
+          modules = [
+            diskoImport ./disko/root
+            ./hosts/generic
+          ];
         };
-        # For secrets management.
-        #agenix = {
-        #    url = "github:ryantm/agenix";
-        #    inputs.nixpkgs.follows = "nixpkgs";
-        #};
-        # For Macs.
-        nix-darwin = {
-            url = "github:LnL7/nix-darwin/master";
-            inputs.nixpkgs.follows = "nixpkgs";
+
+        saeko = {
+          modules = [
+            diskoImport ./disko/root
+            ./hosts/saeko
+          ];
         };
-        # For all home users on client devices.
-        home-manager = {
-            url = "github:nix-community/home-manager/master";
-            inputs.nixpkgs.follows = "nixpkgs";
+
+        saejima = {
+          modules = [
+            diskoImport ./disko/root
+            ./modules/pipewire.nix
+            ./modules/gnome.nix
+            ./modules/gaming.nix
+            ./modules/nvidia.nix
+            ./hosts/saejima
+            home-manager.nixosModules.home-manager micahHomeConfig
+          ];
         };
-        impermanence = {
-            url = "github:nix-community/impermanence";
-            #inputs.nixpkgs.follows = "nixpkgs";
+
+        nanba = {
+          modules = [
+            diskoImport ./disko/root
+            ./hosts/nanba
+          ];
         };
+
+        kaito = {
+          modules = [
+            diskoImport ./disko/imperm-root
+            inputs.impermanence.nixosModules.impermanence
+            ./hosts/configuration.nix
+            ./hosts/kaito
+          ];
+        };
+      };
+
+      nixosSystemConfig = name: cfg: nixpkgs.lib.nixosSystem {
+        system = defaultSystem;
+        modules = (if name == "kaito" then [] else commonModules) ++ cfg.modules;
+      };
+    in {
+      nixosConfigurations =
+        lib.mapAttrs (host: cfg: nixosSystemConfig host cfg) hosts;
+
+      homeConfigurations = {
+        micah = home-manager.lib.homeManagerConfiguration {
+          pkgs = import nixpkgs { system = defaultSystem; };
+          modules = [
+            ./home-manager/clients/linux.nix
+            ./home-manager/clients/home.nix
+          ];
+        };
+      };
+
+      darwinConfigurations = {
+        haruka = nix-darwin.lib.darwinSystem {
+          system = "aarch64-darwin";
+          modules = [
+            ./home-manager/clients/darwin.nix
+            home-manager.darwinModules.home-manager micahHomeConfig
+          ];
+        };
+      };
     };
-
-    outputs = 
-        {
-            self,
-            nixpkgs,
-            nix-darwin,
-            home-manager,
-            #agenix,
-            ...
-        } @inputs: 
-        let
-            modules = import ./modules/default.nix { inherit (nixpkgs) lib; };
-        in 
-        {
-            # darwinConfigurations {};
-            nixosConfigurations = {
-                generic = nixpkgs.lib.nixosSystem {
-                    system = "x86_64-linux";
-                    modules = [
-                        inputs.disko.nixosModules.disko
-                        (import ./disko/root { device = "IDK"; })
-                        ./modules/zfs.nix
-                        ./hosts/configuration.nix
-                        #agenix.nixosModules.default
-                        ./modules/zfs-fs-config.nix
-                        ./hosts/generic
-                    ];
-                };
-                saeko = nixpkgs.lib.nixosSystem {
-                    system = "x86_64-linux";
-                    modules = [
-                        inputs.disko.nixosModules.disko
-                        (import ./disko/root { device = "IDK"; })
-                        ./modules/zfs.nix
-                        ./hosts/configuration.nix
-                        #agenix.nixosModules.default
-                        ./modules/zfs-fs-config.nix
-                        ./hosts/saeko
-                    ];
-                };
-
-                saejima = nixpkgs.lib.nixosSystem {
-                    system = "x86_64-linux";
-                    modules = [
-                        inputs.disko.nixosModules.disko
-                        (import ./disko/root { device = "IDK"; })
-                        ./modules/zfs.nix
-                        ./modules/pipewire.nix
-                        ./modules/gnome.nix
-                        ./modules/gaming.nix
-                        ./modules/nvidia.nix
-                        # Jesus Christ. There's gotta be a cleaner way to do this.
-                        ./hosts/configuration.nix
-                        ./modules/zfs-fs-config.nix
-                        ./hosts/saejima
-                        home-manager.nixosModules.home-manager
-                        {
-                            # NixOS with Home Manager integration
-                            home-manager.useGlobalPkgs = true;
-                            home-manager.useUserPackages = true;
-                            home-manager.users.micah = import ./home-manager/clients/home.nix;
-
-                        }
-              
-                    ];
-                };
-
-                nanba = nixpkgs.lib.nixosSystem {
-                    system = "x86_64-linux";
-                    modules = [
-                        inputs.disko.nixosModules.disko
-                        (import ./disko/root { device = "IDK"; })
-                        ./hosts/configuration.nix
-                        ./modules/zfs-fs-config.nix
-                        ./hosts/nanba
-                    ];
-                };
-
-                kaito = nixpkgs.lib.nixosSystem { # Redundant backup NAS, configured for power savings.
-                    system = "x86_64-linux";
-                    modules = [
-                        inputs.disko.nixosModules.disko
-                        inputs.impermanence.nixosModules.impermanence
-                        (import ./disko/imperm-root { device = "IDK"; })
-                        ./hosts/configuration.nix
-                        ./hosts/kaito
-                    ];
-                };
-            };
-
-            homeConfigurations = {
-                micah = home-manager.lib.homeManagerConfiguration {
-                    pkgs = import nixpkgs { system = "x86_64-linux"; };
-                    modules = [
-                        ./home-manager/clients/linux.nix
-                        ./home-manager/clients/home.nix
-                    ];
-                };
-            };
-
-            darwinConfigurations = {
-                haruka = nix-darwin.lib.darwinSystem {
-                        system = "aarch64-darwin";
-                            modules = [
-                                ./home-manager/clients/darwin.nix
-                                home-manager.darwinModules.home-manager
-                                {
-                                    # nix-darwin with Home Manager integration
-                                    home-manager.useGlobalPkgs = true;
-                                    home-manager.useUserPackages = true;
-                                    home-manager.users.micah = import ./home-manager/clients/home.nix;
-                                }
-                        ];
-                };
-            };
-            
-        };
 }
+
