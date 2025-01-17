2373040e4dc06ce6b54b3d5158f497dcc0d7836a
Micah C <kenzan@mailbox.org>
2025-01-17 14:49:55 -0500

    moved to a path from a string

--- modules/sops.nix
+++ modules/sops.nix
@@ -2,7 +2,7 @@
 
     sops.defaultSopsFile = ../secrets/secrets.json;
     sops.defaultSopsFormat = "json";
-    sops.age.keyFile = "../secrets/age/keys.txt";
+    sops.age.keyFile = ../secrets/age/keys.txt;
 
     sops.secrets.tailscaleAuthKey = { };
 
