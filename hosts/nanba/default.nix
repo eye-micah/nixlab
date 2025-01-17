7ca089177416b992bd2ed3a158002129ab1e52d3
Micah C <kenzan@mailbox.org>
2025-01-17 13:02:39 -0500

    unified config

--- hosts/nanba/default.nix
+++ hosts/nanba/default.nix
@@ -6,28 +6,6 @@
     ./services
   ];
 
-  fileSystems."/" = {
-      device = "zroot";
-      fsType = "zfs";
-  };
-
-  fileSystems."/nix" = {
-      device = "zroot/nix";
-      fsType = "zfs";
-  };
-
-  fileSystems."/home" = {
-      device = "zroot/home";
-      fsType = "zfs";
-  };
-
-  fileSystems."/boot" = {
-      device = lib.mkForce "/dev/disk/by-label/NIXESP";
-      fsType = "vfat";
-  };
-
-  swapDevices = [];
-
 
   networking.hostName = "nanba";
   networking.hostId = "41b9e6d2";
