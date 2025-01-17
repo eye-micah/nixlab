2625b0773f2e64c7112deddd80e571cfc1141814
Micah C <kenzan@mailbox.org>
2025-01-17 12:52:14 -0500

    for some reason left the host config??? sorry

--- hosts/generic/default.nix
+++ hosts/generic/default.nix
@@ -2,9 +2,6 @@
 
 {
   
-  imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
-  boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" ];
-
   networking.hostName = "nixos";
   networking.hostId = "41b9e6d9";
   networking.useDHCP = true;
