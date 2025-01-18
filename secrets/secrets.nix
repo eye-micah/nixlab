079b932fd49687612a615eb8a4995ecc242753f8
Micah C <kenzan@mailbox.org>
2025-01-17 21:11:53 -0500

    FUCK

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,6 @@
 let
   haruka = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHFQy6Jw3QC3ADSbNdRZZSTZMOwB7o/+SQatG4Er2gtC";
-  nanba = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKOE/7FC6A2ERGml96UhowuJMJX/iw6N+k1ks0Un3lZ";
+  nanba = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJxtdbmFvZUWxlJ2Wezvr3t8rFvAGIWtfHJcScgmOQGp";
 in
 {
   "tailscaleNanba.age".publicKeys = [ haruka nanba ];
