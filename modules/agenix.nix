e604948205f0eead3ee26110f6c4b66730c7394f
Micah C <kenzan@mailbox.org>
2025-01-17 19:35:02 -0500

    nanba added to their secret

--- modules/agenix.nix
+++
@@ -1,9 +0,0 @@
-{ config, ... }: {
-
-    sops.defaultSopsFile = ../secrets/secrets.json;
-    sops.defaultSopsFormat = "json";
-    sops.age.keyFile = "~/.config/age/keys.txt";
-
-    sops.secrets.tailscaleAuthKey = { };
-
-}
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,8 @@
 let
   haruka = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHFQy6Jw3QC3ADSbNdRZZSTZMOwB7o/+SQatG4Er2gtC";
+  nanba = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIRcmH2DmzMa8eYswUll6q9SJRGU2fDY32hG6yW71v6C";
 in
 {
-  "tailscaleNanba.age".publicKeys = [ haruka ];
+  "tailscaleNanba.age".publicKeys = [ haruka nanba ];
   "tailscaleSaeko.age".publicKeys = [ haruka ];
 }
