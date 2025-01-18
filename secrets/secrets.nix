929a9891f5c3e57acd35b11e399b09a48331b0d3
Micah C <kenzan@mailbox.org>
2025-01-17 21:05:52 -0500

    root ssh key ugh

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,6 @@
 let
   haruka = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHFQy6Jw3QC3ADSbNdRZZSTZMOwB7o/+SQatG4Er2gtC";
-  nanba = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIRcmH2DmzMa8eYswUll6q9SJRGU2fDY32hG6yW71v6C";
+  nanba = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKOE/7FC6A2ERGml96UhowuJMJX/iw6N+k1ks0Un3lZ";
 in
 {
   "tailscaleNanba.age".publicKeys = [ haruka nanba ];
