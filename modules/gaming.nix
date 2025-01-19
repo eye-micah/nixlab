da1ca1c4a69cc746dd63ed8ab6e348827db6124c
Micah Daniel <kenzan@mailbox.org>
2025-01-19 08:09:33 -0500

    xpad service integrates better with suspend

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -12,21 +12,20 @@
 
   boot.kernelModules = [ "xpad" ]; # Needed for my 8BitDo controller.
 
-  
   systemd.services.load-xpad = {
-    description = "Load xpad module on boot and after sleep";
-    wantedBy = [ "default.target" "suspend.target" ];
-    unitConfig = {
-      # Ensure it runs only after system is ready
-      After = "network.target"; 
-    };
+    description = "Load xpad module on boot and after waking from suspend";
+
+    # Enable the service on boot and hook it into suspend/resume
+    wantedBy = [ "multi-user.target" "sleep.target" ];
+    before = [ "suspend.target" "hibernate.target" ];
     serviceConfig = {
       Type = "oneshot";
       ExecStart = "${pkgs.kmod}/bin/modprobe xpad";
     };
-    install.wantedBy = [ "multi-user.target" ]; # Ensure it's active at boot
   };
- 
+
+
+
 
   hardware.uinput.enable = true;
 
