8d14fc97b28ae5b946b5b1490ba43a6246038ee5
Micah Daniel <kenzan@mailbox.org>
2025-01-22 22:01:47 -0500

    WHAT IS WRONG

--- hosts/saeko/services/jellyfin-1.nix
+++ hosts/saeko/services/jellyfin-1.nix
@@ -4,11 +4,8 @@
   };
 
   services.caddy.virtualHosts."jellyfin.${envVars.localDomain}" = {
-    #useACMEHost = "${envVars.localDomain}";
+    useACMEHost = "${envVars.localDomain}";
     extraConfig = ''
-      tls /var/lib/acme/lan.zandyne.xyz/cert.pem /var/lib/acme/lan.zandyne.xyz/key.pem {
-        resolvers 1.1.1.1
-      }
       reverse_proxy http://127.0.0.1:8096
     ''
     ;
