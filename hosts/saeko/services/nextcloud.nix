1c66eaaf0629cd081b676f187faea85390fb8e05
Micah Daniel <kenzan@mailbox.org>
2025-01-22 22:37:16 -0500

    I hate nextcloud jesus christ

--- hosts/saeko/services/nextcloud.nix
+++ hosts/saeko/services/nextcloud.nix
@@ -2,16 +2,20 @@
   services.nextcloud = {
     enable = true;
     configureRedis = true;
-    package = pkgs.nextcloud28;
-    hostname = "nextcloud.${envVars.localDomain}";
+    package = pkgs.nextcloud30; # why
+    hostName = "nextcloud.${envVars.localDomain}";
     config.adminpassFile = "${config.age.secrets.nextcloudPass.path}";
     config.dbtype = "pgsql";
   };
 
+  # WHY IS THERE A HARD DEPENDENCY ON NGINX???
+  services.nginx.enable = true;
+  services.nginx.virtualHosts."localhost".listen = [ { addr = "127.0.0.1"; port = 8080; } ];
+
   services.caddy.virtualHosts."nextcloud.${envVars.localDomain}" = {
     useACMEHost = "${envVars.localDomain}";
     extraConfig = ''
-      reverseProxy http://127.0.0.1:8096
+      reverseProxy http://127.0.0.1:8080
     ''
     ;
   };
