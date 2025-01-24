3a2ba35a01e208991a3f21ac934dfa48cda1cbde
Micah Daniel <micah@micah.com>
2025-01-23 20:01:25 -0500

    nixvim plugins

--- modules/nixvim.nix
+++ modules/nixvim.nix
@@ -8,9 +8,36 @@
       opts = {
         number = true;
       };
+      clipboard.register = "unnamedplus";
       plugins = {
         nix.enable = true;
-        #presence.nvim.enable = true;
+        cmp-fuzzy-path.enable = true;
+        nvim-tree.enable = true;
+        auto-save.enable = true;
+        presence.nvim.enable = true;
+        cmp = {
+          autoEnableSources = true;
+          settings.sources = [
+            { name = "nvim_lsp"; }
+            { name = "path"; }
+            { name = "buffer"; }
+          };
+        treesitter = {
+          enable = true;
+          grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
+            bash
+            json
+            lua
+            make
+            markdown
+            nix
+            regex
+            toml
+            vim
+            vimdoc
+            xml
+            yaml
+          ];
       };
   };
 }
