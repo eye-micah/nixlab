ed185ddce7a5fbd9ad40a2968ba15de86dcbcce7
Micah Daniel <kenzan@mailbox.org>
2025-01-25 11:56:41 -0500

    I HATE THIS

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -16,4 +16,11 @@
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
+
+  boot.initrd.kernelModules = [
+    "nvidia"
+    "nvidia_modeset"
+    "nvidia_uvm"
+    "nvidia_drm"
+  ];
 }
