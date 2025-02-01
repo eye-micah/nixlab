40e8f1bc54c16ad8bab04c637d338d081772076c
Micah Daniel <kenzan@mailbox.org>
2025-02-01 17:06:08 -0500

    did some tweaks

--- modules/homelab/navidrome.nix
+++ modules/homelab/navidrome.nix
@@ -15,3 +15,32 @@
         ;
     };
 }
+
+
+{ lib, config, pkgs, ... }:
+
+with lib;
+
+let
+
+  configOptions = {
+    homelab.navidrome = mkOption {
+      type = types.submodule {
+        options = {
+          enable = mkOption {
+            type = types.bool;
+            default = false;
+            description = "Enable the Navidrome service";
+          };
+
+          enableCaddy = mkOption {
+            type = types.bool;
+            default = true;
+            description = "Enable the Caddy virtual host for Navidrome.";
+          };
+        };
+      };
+      default = { enable = false; enableCaddy = false; };
+      description = "Options for enabling Navidrome and Caddy configuration.";
+    };
+    
